2f6f0a7df9a443fea90bd14b25eb5108885daa66
Jonas Góes <jonasdsg1@gmail.com>
2024-01-28 17:35:48 -0300

    Initial commit without coding

---
+++ src/test/java/io/gitihub/jonasdsg/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gitihub.jonasdsg.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
